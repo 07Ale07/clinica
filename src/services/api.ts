833828e32bb58009a6eb0ccdcd6db0c5183bb0f0
Solange777 <solangedeutz@gmail.com>
2025-10-31 10:08:28 -0300

    quiero moverme de rama

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@
 import axios, { AxiosResponse } from 'axios';
 import { LoginResponse, LoginCredentials } from '../../modelo/LoginModel';
 
-export const API_BASE_URL = 'http://192.168.0.19:3000';
+export const API_BASE_URL = 'http://10.0.10.117:3000';
 
 export const apiService = {
   async login(credentials: LoginCredentials): Promise<LoginResponse> {
